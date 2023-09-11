a956d8cab256adc1dbf7d9af1d8feaa9174dab25
the_FallenAngel <the.fallenangel.965@gmail.com>
2023-09-11 14:50:25 +0800

    wip: 新增括号的配对匹配

---
+++ src/main/kotlin/net/fallingangel/jimmerdto/completion/DTOBraceMatcher.kt
@@ -0,0 +1,26 @@
+package net.fallingangel.jimmerdto.completion
+
+import com.intellij.lang.BracePair
+import com.intellij.lang.PairedBraceMatcher
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.psi.PsiFile
+import com.intellij.psi.tree.IElementType
+import net.fallingangel.jimmerdto.psi.DTOTypes
+
+class DTOBraceMatcher : PairedBraceMatcher {
+    override fun getPairs() = arrayOf(
+        BracePair(DTOTypes.BRACE_L, DTOTypes.BRACE_R, true),
+        BracePair(DTOTypes.PAREN_L, DTOTypes.PAREN_R, false),
+        BracePair(DTOTypes.BRACKET_L, DTOTypes.BRACKET_R, false),
+        BracePair(DTOTypes.ANGLE_BRACKET_L, DTOTypes.ANGLE_BRACKET_R, false)
+    )
+
+    override fun isPairedBracesAllowedBeforeType(lbraceType: IElementType, contextType: IElementType?) = true
+
+    override fun getCodeConstructStart(file: PsiFile, openingBraceOffset: Int): Int {
+        val logger = logger<DTOBraceMatcher>()
+        logger.info("getCodeConstructStart: $openingBraceOffset")
+        logger.info(file.text)
+        return openingBraceOffset
+    }
+}
