74e59b3447fc402c77d57a19e65e68743b3209f2
the_FallenAngel <the.fallenangel.965@gmail.com>
2023-10-17 22:21:52 +0800

    fix: 调整实体的父级实体提示

--- src/main/kotlin/net/fallingangel/jimmerdto/completion/resolve/structure/DtoSupers.kt
+++ src/main/kotlin/net/fallingangel/jimmerdto/completion/resolve/structure/DtoSupers.kt
@@ -2,6 +2,7 @@ package net.fallingangel.jimmerdto.completion.resolve.structure
 
 import net.fallingangel.jimmerdto.enums.Modifier
 import net.fallingangel.jimmerdto.enums.modifiedBy
+import net.fallingangel.jimmerdto.enums.notModifiedBy
 import net.fallingangel.jimmerdto.psi.DTODto
 import net.fallingangel.jimmerdto.psi.DTODtoSupers
 import net.fallingangel.jimmerdto.psi.impl.DTOPsiImplUtil
@@ -13,13 +14,15 @@ class DtoSupers : Structure<DTODtoSupers, List<String>> {
      * @return Dto的可用父级Dto列表
      */
     override fun value(element: DTODtoSupers): List<String> {
-        val supers = DTOPsiImplUtil.findDTOs(element)
         val currentDto = element.parent as DTODto
+        val supers = DTOPsiImplUtil.findDTOs(element).filter { it.dtoName.name != currentDto.dtoName.name }
 
         val availableSupers = if (currentDto modifiedBy Modifier.INPUT) {
             supers.filter { it modifiedBy Modifier.INPUT }
         } else if (currentDto modifiedBy Modifier.INPUT_ONLY) {
             supers.filter { it modifiedBy Modifier.INPUT_ONLY }
+        } else if (currentDto notModifiedBy Modifier.INPUT_ONLY) {
+            supers.filter { it notModifiedBy Modifier.INPUT_ONLY }
         } else {
             supers
         }
