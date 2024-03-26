044b8c5e95ebc78cb7ff37ff509eaa045b16dce9
the_FallenAngel <the.fallenangel.965@gmail.com>
2024-03-26 13:59:59 +0800

    wip: 移除内部Api的使用：NotificationGroup.create

--- src/main/kotlin/net/fallingangel/jimmerdto/action/CreateJimmerDtoFile.kt
+++ src/main/kotlin/net/fallingangel/jimmerdto/action/CreateJimmerDtoFile.kt
@@ -1,14 +1,11 @@
 package net.fallingangel.jimmerdto.action
 
-import com.intellij.notification.NotificationDisplayType
-import com.intellij.notification.NotificationGroup
+import com.intellij.notification.NotificationGroupManager
 import com.intellij.notification.NotificationType
-import com.intellij.notification.Notifications
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.command.WriteCommandAction
-import com.intellij.openapi.extensions.PluginId
 import net.fallingangel.jimmerdto.util.*
 import org.jetbrains.kotlin.idea.core.util.toPsiFile
 import org.jetbrains.kotlin.idea.refactoring.memberInfo.qualifiedClassNameForRendering
@@ -17,23 +14,13 @@ class CreateJimmerDtoFile : AnAction() {
     override fun actionPerformed(event: AnActionEvent) {
         val project = event.project!!
 
-        @Suppress("UnstableApiUsage")
-        val notificationGroup = NotificationGroup.create(
-            "JimmerDTONotificationGroup",
-            NotificationDisplayType.BALLOON,
-            true,
-            "",
-            "Deprecated",
-            PluginId.findId("JimmerDTO")
-        )
-
-        val notification = notificationGroup.createNotification(
-            "Warning",
-            "Since the Dto language supports 'export', this operation is obsolete and will be removed in 0.0.8, so don't use it anymore!",
-            NotificationType.WARNING
-        )
-
-        Notifications.Bus.notify(notification, project)
+        NotificationGroupManager.getInstance()
+                .getNotificationGroup("JimmerDTO Notification Group")
+                .createNotification(
+                    "Since the Dto language supports 'export', this operation is obsolete and will be removed in 0.0.8, so don't use it anymore!",
+                    NotificationType.WARNING
+                )
+                .notify(project)
 
         val entityFile = event.getData(CommonDataKeys.VIRTUAL_FILE) ?: return
         val qualifiedEntityName = entityFile.nameIdentifier(project)?.qualifiedClassNameForRendering() ?: return
