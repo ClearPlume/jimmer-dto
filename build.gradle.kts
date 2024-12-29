1415f645e0367c40a4c165744c350991e77c48fd
the_FallingAngel <the.fallenangel.965@gmail.com>
2024-12-29 13:40:35 +0800

    chore: 调整项目配置

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,17 @@
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.8.22"
-    id("org.jetbrains.intellij") version "1.17.3"
+    id("org.jetbrains.intellij.platform") version "2.2.1"
 }
 
 group = "net.fallingangel"
 version = "0.0.7.28"
 
-val sinceVersion by extra("223.7571.182")
-val untilVersion by extra("243.*")
+val sinceVersion by extra("2022.3")
+val untilVersion by extra("2024.3")
 val jimmerVersion by extra("0.8.150")
 
 val certificateChainValue = findProperty("certificateChainValue") as String?
@@ -22,24 +23,56 @@ sourceSets["main"].java.srcDir("src/main/gen")
 
 repositories {
     mavenCentral()
+
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
 dependencies {
+    intellijPlatform {
+        create("IC", sinceVersion)
+        bundledPlugins("com.intellij.java", "org.jetbrains.kotlin")
+
+        testFramework(TestFrameworkType.Platform)
+    }
+
+    testImplementation("junit:junit:4.13.2")
     testCompileOnly("org.babyfish.jimmer:jimmer-sql-kotlin:$jimmerVersion")
 }
 
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-    pluginName.set("JimmerDTO")
-    version.set("2022.3")
-    type.set("IC") // Target IDE Platform
-    plugins.set(
-        listOf(
-            "com.intellij.java",
-            "org.jetbrains.kotlin"
-        )
-    )
+intellijPlatform {
+    pluginConfiguration {
+        id = "net.fallingangel.jimmer-dto"
+        name = "JimmerDTO"
+
+        vendor {
+            name = "the_FallenAngel"
+            email = "the.fallenangel.965@gmail.com"
+            url = "https://fallingangel.net"
+        }
+
+        ideaVersion {
+            sinceBuild = "223"
+        }
+    }
+
+    pluginVerification {
+        ides {
+            ide("IC", sinceVersion)
+        }
+    }
+
+    publishing {
+        token = tokenValue
+        channels.add("Stable")
+    }
+
+    signing {
+        certificateChain = certificateChainValue
+        privateKey = privateKeyValue
+        password = passwordValue
+    }
 }
 
 tasks {
@@ -56,26 +89,10 @@ tasks {
     }
 
     test {
-        systemProperty("idea.home.path", intellij.sandboxDir.get())
-    }
-
-    patchPluginXml {
-        sinceBuild.set(sinceVersion)
-        untilBuild.set(untilVersion)
+        systemProperty("idea.home.path", intellijPlatform.sandboxContainer.get().toString())
     }
 
     runIde {
         jvmArgs("-Xms128m", "-Xmx4096m", "-XX:ReservedCodeCacheSize=512m")
-    }
-
-    signPlugin {
-        certificateChain.set(certificateChainValue)
-        privateKey.set(privateKeyValue)
-        password.set(passwordValue)
-    }
-
-    publishPlugin {
-        channels.add("Stable")
-        token.set(tokenValue)
     }
 }
