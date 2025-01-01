fa04b7367d0f92bfc51c391bd03669d24131ca55
the_FallingAngel <the.fallenangel.965@gmail.com>
2025-01-01 16:32:39 +0800

    fix: 修复无法识别kt文件中KtClass

--- src/main/kotlin/net/fallingangel/jimmerdto/util/AnnotationClassIndex.kt
+++ src/main/kotlin/net/fallingangel/jimmerdto/util/AnnotationClassIndex.kt
@@ -1,11 +1,13 @@
 package net.fallingangel.jimmerdto.util
 
+import com.intellij.ide.highlighter.JavaFileType
 import com.intellij.psi.PsiClass
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.util.indexing.*
 import com.intellij.util.io.EnumeratorStringDescriptor
 import org.jetbrains.kotlin.idea.util.isJavaFileType
 import org.jetbrains.kotlin.idea.util.isKotlinFileType
+import org.jetbrains.kotlin.psi.KtClass
 
 val ANNOTATION_CLASS_INDEX = ID.create<String, Void>("net.fallingangel.jimmerdto.util.AnnotationClassIndex")
 
@@ -13,9 +15,15 @@ class AnnotationClassIndex : ScalarIndexExtension<String>() {
     override fun getName() = ANNOTATION_CLASS_INDEX
 
     override fun getIndexer() = DataIndexer<String, Void, FileContent> { file ->
-        PsiTreeUtil.findChildrenOfType(file.psiFile, PsiClass::class.java)
-                .filter { it.isAnnotationType }
-                .associate { Pair(it.qualifiedName, null) }
+        if (file.fileType == JavaFileType.INSTANCE) {
+            PsiTreeUtil.findChildrenOfType(file.psiFile, PsiClass::class.java)
+                    .filter { it.isAnnotationType }
+                    .associate { Pair(it.qualifiedName, null) }
+        } else {
+            PsiTreeUtil.findChildrenOfType(file.psiFile, KtClass::class.java)
+                    .filter { it.isAnnotation() }
+                    .associate { Pair(it.fqName!!.asString(), null) }
+        }
     }
 
     override fun getKeyDescriptor() = EnumeratorStringDescriptor()
