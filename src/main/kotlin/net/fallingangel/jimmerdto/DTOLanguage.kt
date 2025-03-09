d53d9c2925a52e02e3feaad24d00bafe01545e1e
the_FallingAngel <the.fallenangel.965@gmail.com>
2025-03-09 17:29:12 +0800

    feat: DTOLanguage#preludes

--- src/main/kotlin/net/fallingangel/jimmerdto/DTOLanguage.kt
+++ src/main/kotlin/net/fallingangel/jimmerdto/DTOLanguage.kt
@@ -2,9 +2,17 @@ package net.fallingangel.jimmerdto
 
 import com.intellij.lang.Language
 import net.fallingangel.jimmerdto.psi.DTOParser
+import net.fallingangel.jimmerdto.structure.BasicType
+import net.fallingangel.jimmerdto.structure.GenericType
 import org.antlr.intellij.adaptor.lexer.PSIElementTypeFactory
 
 object DTOLanguage : Language(Constant.NAME) {
+    val preludes by lazy {
+        val basicTypes = BasicType.types()
+        val genericTypes = GenericType.types().map { it.presentation }
+        basicTypes + genericTypes
+    }
+
     init {
         val vocab = DTOParser.VOCABULARY
 
