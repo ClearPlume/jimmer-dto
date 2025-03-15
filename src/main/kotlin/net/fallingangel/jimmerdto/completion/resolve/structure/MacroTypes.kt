ba8dbb226f93bef171f4a1d15f8e3d5f1a442882
the_FallingAngel <the.fallenangel.965@gmail.com>
2025-03-15 18:46:42 +0800

    feat: 宏名称快速文档

--- src/main/kotlin/net/fallingangel/jimmerdto/completion/resolve/structure/MacroTypes.kt
+++ src/main/kotlin/net/fallingangel/jimmerdto/completion/resolve/structure/MacroTypes.kt
@@ -1,17 +1,17 @@
 package net.fallingangel.jimmerdto.completion.resolve.structure
 
 import net.fallingangel.jimmerdto.lsi.LClass
-import net.fallingangel.jimmerdto.psi.element.DTOMacroArgs
+import net.fallingangel.jimmerdto.psi.element.DTOMacro
 import net.fallingangel.jimmerdto.util.*
 
-class MacroTypes : Structure<DTOMacroArgs, List<String>> {
+class MacroTypes : Structure<DTOMacro, List<String>> {
     /**
      * @param element Dto宏的参数元素
      *
      * @return 宏的可用参数类型列表
      */
-    override fun value(element: DTOMacroArgs): List<String> {
-        val propPath = element.parent.propPath()
+    override fun value(element: DTOMacro): List<String> {
+        val propPath = element.propPath()
         val clazz = element.file.clazz.walk(propPath)
         return clazz.allParents.map(LClass<*>::name) + clazz.name + "this"
     }
