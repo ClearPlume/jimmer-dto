2313f5b5db490eea35064c966221d4ff7397d910
the_FallingAngel <the.fallenangel.965@gmail.com>
2025-03-17 17:47:35 +0800

    feat: DTONamedElementImpl#getReference

--- src/main/kotlin/net/fallingangel/jimmerdto/psi/mixin/impl/DTONamedElementImpl.kt
+++ src/main/kotlin/net/fallingangel/jimmerdto/psi/mixin/impl/DTONamedElementImpl.kt
@@ -2,16 +2,11 @@ package net.fallingangel.jimmerdto.psi.mixin.impl
 
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiReference
-import com.intellij.psi.impl.source.resolve.reference.ReferenceProvidersRegistry
 import net.fallingangel.jimmerdto.psi.mixin.DTONamedElement
+import net.fallingangel.jimmerdto.refenerce.DTOReference
 import org.antlr.intellij.adaptor.psi.ANTLRPsiNode
 
 abstract class DTONamedElementImpl(node: ASTNode) : ANTLRPsiNode(node), DTONamedElement {
-    override fun getReferences(): Array<PsiReference> {
-        return ReferenceProvidersRegistry.getReferencesFromProviders(this)
-    }
-
     override fun getName() = nameIdentifier?.text
 
     override fun setName(name: String): DTONamedElement {
@@ -20,6 +15,8 @@ abstract class DTONamedElementImpl(node: ASTNode) : ANTLRPsiNode(node), DTONamed
     }
 
     protected open fun newNameNode(name: String): ASTNode? = null
+
+    override fun getReference() = DTOReference(this, firstChild.textRangeInParent)
 
     override fun resolve(): PsiElement? {
         return null
