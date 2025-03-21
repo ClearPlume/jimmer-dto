6c6fd75555a91eebab45640a87dae0bd7b15d7d5
the_FallingAngel <the.fallenangel.965@gmail.com>
2025-03-21 14:11:45 +0800

    feat: DTOBraceMatcher

--- src/main/kotlin/net/fallingangel/jimmerdto/completion/DTOBraceMatcher.kt
+++ src/main/kotlin/net/fallingangel/jimmerdto/completion/DTOBraceMatcher.kt
@@ -5,14 +5,14 @@ import com.intellij.lang.PairedBraceMatcher
 import com.intellij.psi.PsiFile
 import com.intellij.psi.tree.IElementType
 import net.fallingangel.jimmerdto.DTOLanguage.token
-import net.fallingangel.jimmerdto.psi.DTOLexer
+import net.fallingangel.jimmerdto.psi.DTOParser.*
 
 class DTOBraceMatcher : PairedBraceMatcher {
     override fun getPairs() = arrayOf(
-        BracePair(token[DTOLexer.LBrace], token[DTOLexer.RBrace], true),
-        BracePair(token[DTOLexer.LParen], token[DTOLexer.RParen], true),
-        BracePair(token[DTOLexer.LBracket], token[DTOLexer.RBracket], true),
-        BracePair(token[DTOLexer.LessThan], token[DTOLexer.GreaterThan], true),
+        BracePair(token[LBrace], token[RBrace], true),
+        BracePair(token[LParen], token[RParen], true),
+        BracePair(token[LBracket], token[RBracket], true),
+        BracePair(token[LessThan], token[GreaterThan], true),
     )
 
     override fun isPairedBracesAllowedBeforeType(lbraceType: IElementType, contextType: IElementType?) = true
