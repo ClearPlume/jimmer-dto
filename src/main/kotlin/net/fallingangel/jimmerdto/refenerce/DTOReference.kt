8ad5b42f742ccc7654008df8ab93d35e142a8bb7
the_FallingAngel <the.fallenangel.965@gmail.com>
2025-03-30 22:19:29 +0800

    feat: 索引未就绪时不尝试解析引用

--- src/main/kotlin/net/fallingangel/jimmerdto/refenerce/DTOReference.kt
+++ src/main/kotlin/net/fallingangel/jimmerdto/refenerce/DTOReference.kt
@@ -1,12 +1,18 @@
 package net.fallingangel.jimmerdto.refenerce
 
+import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiReferenceBase
 import net.fallingangel.jimmerdto.psi.mixin.DTONamedElement
 
 class DTOReference(private val element: DTONamedElement, textRange: TextRange) : PsiReferenceBase<PsiElement>(element, textRange) {
-    override fun resolve() = element.resolve()
+    override fun resolve(): PsiElement? {
+        if (DumbService.getInstance(element.project).isDumb) {
+            return null
+        }
+        return element.resolve()
+    }
 
     override fun handleElementRename(newElementName: String): PsiElement {
         return element.setName(newElementName)
