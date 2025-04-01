395a5cf6f689ffd8a213d0469ea35090782ca6dd
the_FallingAngel <the.fallenangel.965@gmail.com>
2025-04-01 12:12:28 +0800

    feat: 快速修复使用Editor.popupChooser

--- src/main/kotlin/net/fallingangel/jimmerdto/psi/fix/ImportClass.kt
+++ src/main/kotlin/net/fallingangel/jimmerdto/psi/fix/ImportClass.kt
@@ -3,21 +3,16 @@ package net.fallingangel.jimmerdto.psi.fix
 import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.ui.popup.JBPopupListener
-import com.intellij.openapi.ui.popup.LightweightWindowEvent
-import com.intellij.openapi.ui.popup.PopupChooserBuilder
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
-import com.intellij.psi.TokenType
 import com.intellij.psi.search.ProjectScope
 import com.intellij.psi.search.PsiShortNamesCache
-import com.intellij.ui.components.JBList
-import net.fallingangel.jimmerdto.psi.element.DTODto
 import net.fallingangel.jimmerdto.psi.element.DTOImportStatement
 import net.fallingangel.jimmerdto.psi.element.DTOQualifiedName
 import net.fallingangel.jimmerdto.psi.element.createImport
 import net.fallingangel.jimmerdto.util.findChild
 import net.fallingangel.jimmerdto.util.findChildNullable
+import net.fallingangel.jimmerdto.util.popupChooser
 
 class ImportClass(private val element: DTOQualifiedName) : BaseFix() {
     override fun getText() = "Import class"
@@ -32,34 +27,15 @@ class ImportClass(private val element: DTOQualifiedName) : BaseFix() {
         val classes = PsiShortNamesCache.getInstance(project)
                 .getClassesByName(element.text, ProjectScope.getAllScope(project))
                 .mapNotNull { it.qualifiedName }
-        val classesHolder = JBList(classes)
+        val dtoFile = file.findChild<PsiElement>("/dtoFile")
 
-        val classChooser = PopupChooserBuilder(classesHolder)
-                .setTitle("Class to Import")
-                .createPopup()
-        classChooser.showInBestPositionFor(editor)
-        classChooser.addListener(object : JBPopupListener {
-            override fun onClosed(event: LightweightWindowEvent) {
-                if (event.isOk) {
-                    val dtoFile = file.findChild<PsiElement>("/dtoFile")
-                    val selectedClass = classesHolder.selectedValue
-                    val dtoImport = project.createImport(selectedClass)
+        editor.popupChooser("Class to Import", classes) {
+            val newImport = project.createImport(it)
+            val oldImport = file.findChildNullable<DTOImportStatement>("/dtoFile/importStatement")
 
-                    val import = file.findChildNullable<DTOImportStatement>("/dtoFile/importStatement")
-                    if (import != null) {
-                        WriteCommandAction.runWriteCommandAction(project) {
-                            dtoFile.node.addChild(dtoImport.node, import.node)
-                            dtoFile.node.addLeaf(TokenType.WHITE_SPACE, "\n", import.node)
-                        }
-                    } else {
-                        val dto = file.findChild<DTODto>("/dtoFile/dto")
-                        WriteCommandAction.runWriteCommandAction(project) {
-                            dtoFile.node.addChild(dtoImport.node, dto.node)
-                            dtoFile.node.addLeaf(TokenType.WHITE_SPACE, "\n\n", dto.node)
-                        }
-                    }
-                }
+            WriteCommandAction.runWriteCommandAction(project) {
+                dtoFile.addBefore(newImport, oldImport ?: file.findChild("/dtoFile/dto"))
             }
-        })
+        }
     }
 }
--- src/main/kotlin/net/fallingangel/jimmerdto/psi/fix/InsertMacroArg.kt
+++ src/main/kotlin/net/fallingangel/jimmerdto/psi/fix/InsertMacroArg.kt
@@ -3,37 +3,21 @@ package net.fallingangel.jimmerdto.psi.fix
 import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.ui.popup.JBPopupListener
-import com.intellij.openapi.ui.popup.LightweightWindowEvent
-import com.intellij.openapi.ui.popup.PopupChooserBuilder
 import com.intellij.psi.PsiFile
-import com.intellij.ui.components.JBList
 import net.fallingangel.jimmerdto.psi.element.DTOMacro
 import net.fallingangel.jimmerdto.psi.element.createMacroArg
+import net.fallingangel.jimmerdto.util.popupChooser
 
 class InsertMacroArg(private val element: DTOMacro) : BaseFix() {
     override fun getText() = "Insert macro arg"
 
     override fun invoke(project: Project, editor: Editor, file: PsiFile) {
         val args = element.args ?: return
-        val macroAvailableArgs = element.types
-        val argsHolder = JBList(macroAvailableArgs)
 
-        val argChooser = PopupChooserBuilder(argsHolder)
-                .setTitle("Macro Arg to Import")
-                .createPopup()
-        argChooser.showInBestPositionFor(editor)
-        argChooser.addListener(object : JBPopupListener {
-            override fun onClosed(event: LightweightWindowEvent) {
-                if (event.isOk) {
-                    val selectedArg = argsHolder.selectedValue
-                    val arg = project.createMacroArg(selectedArg)
-
-                    WriteCommandAction.runWriteCommandAction(project) {
-                        args.node.addChild(arg.node, args.r.node)
-                    }
-                }
+        editor.popupChooser("Macro Arg to Insert", element.types) {
+            WriteCommandAction.runWriteCommandAction(project) {
+                args.addBefore(project.createMacroArg(it), args.r)
             }
-        })
+        }
     }
 }
