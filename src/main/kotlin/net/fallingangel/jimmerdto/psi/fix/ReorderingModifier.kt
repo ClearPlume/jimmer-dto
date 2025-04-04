718ca77b42c812df06658f2a9ae537637c524784
the_FallingAngel <the.fallenangel.965@gmail.com>
2025-04-04 18:46:20 +0800

    fix: ReorderingModifier差异补正

--- src/main/kotlin/net/fallingangel/jimmerdto/psi/fix/ReorderingModifier.kt
+++ src/main/kotlin/net/fallingangel/jimmerdto/psi/fix/ReorderingModifier.kt
@@ -1,21 +1,22 @@
 package net.fallingangel.jimmerdto.psi.fix
 
-import com.intellij.modcommand.ActionContext
-import com.intellij.modcommand.ModPsiUpdater
-import com.intellij.modcommand.PsiUpdateModCommandAction
+import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiFile
 import net.fallingangel.jimmerdto.psi.element.DTODto
 import net.fallingangel.jimmerdto.psi.element.createModifier
 
-@Suppress("UnstableApiUsage")
-class ReorderingModifier(dto: DTODto) : PsiUpdateModCommandAction<DTODto>(dto) {
-    override fun getFamilyName() = "Reordering modifiers"
+class ReorderingModifier(private val dto: DTODto) : BaseFix() {
+    override fun getText() = "Reordering modifiers"
 
-    override fun invoke(context: ActionContext, element: DTODto, updater: ModPsiUpdater) {
-        val project = context.project
-        val orderedModifiers = element.modifierElements.zip(element.modifiers)
+    override fun invoke(project: Project, editor: Editor, file: PsiFile) {
+        val orderedModifiers = dto.modifierElements.zip(dto.modifiers)
                 .sortedBy { it.second.order }
                 .map { project.createModifier(it.first.text) }
-        element.modifierElements.zip(orderedModifiers)
-                .forEach { (modifier, orderedModifier) -> modifier.replace(orderedModifier) }
+        WriteCommandAction.runWriteCommandAction(project) {
+            dto.modifierElements.zip(orderedModifiers)
+                    .forEach { (modifier, orderedModifier) -> modifier.replace(orderedModifier) }
+        }
     }
 }
