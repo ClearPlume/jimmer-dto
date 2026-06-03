d3e5c6f28eb6f921bceef076edf321d431b61e2e
FallingAngel <the.fallenangel.965@gmail.com>
2026-06-03 17:49:00 +0800

    refactor(build): 重整签名和发布细节
    (cherry picked from commit 1668902450f075233715f2013b0a90d3fd4c8eaa)

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,10 +21,9 @@ val until by extra("241.*")
 val jimmerVersion by extra("0.10.6")
 val antlrVersion by extra("4.13.2")
 
-val certificateChainValue = findProperty("certificateChainValue") as String?
-val privateKeyValue = findProperty("privateKeyValue") as String?
-val passwordValue = findProperty("passwordValue") as String?
-val tokenValue = findProperty("tokenValue") as String?
+val userHome: String = System.getProperty("user.home")
+val certificateChainFileValue = file("$userHome/.gradle/chain.crt")
+val privateKeyFileValue = file("$userHome/.gradle/private.pem")
 
 repositories {
     mavenCentral()
@@ -98,15 +97,15 @@ intellijPlatform {
         ides.recommended()
     }
 
-    publishing {
-        token = tokenValue
-        channels.add("Stable")
+    signing {
+        certificateChainFile = certificateChainFileValue
+        privateKeyFile = privateKeyFileValue
+        password = providers.gradleProperty("intellijPlatformSigningPassword")
     }
 
-    signing {
-        certificateChain = certificateChainValue
-        privateKey = privateKeyValue
-        password = passwordValue
+    publishing {
+        token = providers.gradleProperty("intellijPlatformPublishingToken")
+        channels.add("Stable")
     }
 }
 
