045f268e1d857d9f84dd3b17fde12c59427c0ff3
FallingAngel <the.fallenangel.965@gmail.com>
2026-07-10 21:09:12 +0800

    fix(annotator): 为宏名非法、重复定义与可选标识多余的错误添加提前返回以避免重复标注与后续校验干扰并降低多次提示造成的噪声

--- src/main/kotlin/net/fallingangel/jimmerdto/highlighting/DTOAnnotator.kt
+++ src/main/kotlin/net/fallingangel/jimmerdto/highlighting/DTOAnnotator.kt
@@ -473,20 +473,23 @@ class DTOAnnotator : Annotator {
                     "Macro name should be \"allScalars\" or \"allReferences\"",
                     ChooseMacro(macroName),
                 )
+                return
             }
 
-            // 宏的重复定义
             val parent = o.parent
             val macros = if (parent is DTODtoBody) {
                 parent.macros
             } else {
                 parent.findChildren("/aliasGroupBody/macro")
             }
+
+            // 宏的重复定义
             if (macros.count { it.name.value == macroName.value } > 1) {
                 o.error(
                     "Duplicated macro ${macroName.value}",
                     RemoveElement(macroName.value, o),
                 )
+                return
             }
 
             // 宏可选标识在specification中不再需要
@@ -497,6 +500,7 @@ class DTOAnnotator : Annotator {
                         "Unnecessary optional modifier `?`",
                         RemoveElement("?", it),
                     )
+                    return
                 }
             }
         }
