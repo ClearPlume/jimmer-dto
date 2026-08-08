89dbe5b3e100556ffaf93e015c721abe3382a9bb
FallingAngel <the.fallenangel.965@gmail.com>
2026-08-08 18:25:21 +0800

    fix: 禁止重命名 DTO 函数属性

--- src/main/kotlin/net/fallingangel/jimmerdto/refactor/DTORenameCondition.kt
+++ src/main/kotlin/net/fallingangel/jimmerdto/refactor/DTORenameCondition.kt
@@ -2,25 +2,14 @@ package net.fallingangel.jimmerdto.refactor
 
 import com.intellij.openapi.util.Condition
 import com.intellij.psi.PsiElement
-import com.intellij.refactoring.RefactoringBundle
-import com.intellij.refactoring.util.CommonRefactoringUtil
 import net.fallingangel.jimmerdto.psi.element.DTOMacroName
-import org.jetbrains.kotlin.idea.codeinsight.utils.findExistingEditor
+import net.fallingangel.jimmerdto.psi.element.DTOPositiveProp
 
 /**
  * 元素重命名控制条件，返回true则禁止重命名
  */
 class DTORenameCondition : Condition<PsiElement> {
     override fun value(element: PsiElement?): Boolean {
-        if (element is DTOMacroName) {
-            CommonRefactoringUtil.showErrorHint(
-                element.project,
-                element.findExistingEditor(),
-                RefactoringBundle.message("error.cannot.be.renamed"),
-                RefactoringBundle.message("rename.title"),
-                null,
-            )
-        }
-        return element is DTOMacroName
+        return element is DTOMacroName || (element?.parent as? DTOPositiveProp)?.arg != null
     }
 }
