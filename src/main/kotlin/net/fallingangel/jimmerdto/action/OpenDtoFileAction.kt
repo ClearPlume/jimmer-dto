a919bf894f7cc79d76b9009aad8413f362d749ee
FallingAngel <the.fallenangel.965@gmail.com>
2026-08-09 13:17:54 +0800

    feat(action): 从实体跳转 DTO 时，若实体对应多个 DTO 文件，弹出选择列表

--- src/main/kotlin/net/fallingangel/jimmerdto/action/OpenDtoFileAction.kt
+++ src/main/kotlin/net/fallingangel/jimmerdto/action/OpenDtoFileAction.kt
@@ -1,18 +1,25 @@
 package net.fallingangel.jimmerdto.action
 
+import com.intellij.codeInsight.navigation.PsiTargetNavigator
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.openapi.ui.popup.JBPopupFactory
 import com.intellij.openapi.vfs.findDirectory
 import com.intellij.openapi.vfs.findFile
 import com.intellij.openapi.vfs.findOrCreateFile
+import com.intellij.util.indexing.FileBasedIndex
+import net.fallingangel.jimmerdto.index.DTO_ENTITY_INDEX
 import net.fallingangel.jimmerdto.lsi.LKind
 import net.fallingangel.jimmerdto.lsi.jimmer.JimmerAnnotations
 import net.fallingangel.jimmerdto.lsi.process
+import net.fallingangel.jimmerdto.psi.DTOFile
 import net.fallingangel.jimmerdto.util.open
+import org.jetbrains.kotlin.idea.base.util.projectScope
+import org.jetbrains.kotlin.idea.core.util.toPsiFile
 
 class OpenDtoFileAction : AnAction() {
     override fun actionPerformed(event: AnActionEvent) {
@@ -21,6 +28,31 @@ class OpenDtoFileAction : AnAction() {
         val entityElement = process(selectedElement.containingFile) { topLevelClasses() }?.singleOrNull() ?: return
 
         val entityQualifiedName = process(entityElement) { classQualifiedName() } ?: return
+
+        val dtoFiles = FileBasedIndex.getInstance()
+            .getContainingFiles(DTO_ENTITY_INDEX, entityQualifiedName, project.projectScope())
+            .mapNotNull { it.toPsiFile(project) }
+        if (dtoFiles.size == 1) {
+            dtoFiles.single().navigate(true)
+            return
+        }
+
+        if (dtoFiles.size > 1) {
+            val point = JBPopupFactory.getInstance().guessBestPopupLocation(event.dataContext)
+            PsiTargetNavigator(dtoFiles)
+                .builderConsumer { builder ->
+                    builder.setNamerForFiltering {
+                        val file = it.dereference() as DTOFile
+                        file.name
+                    }
+                }
+                .navigate(point, "Choose DTO", project) {
+                    it.navigate(true)
+                    true
+                }
+            return
+        }
+
         val entityPackage = entityQualifiedName.substringBeforeLast('.', "")
         val entityName = entityQualifiedName.substringAfterLast('.')
         val dtoFileName = buildString {
