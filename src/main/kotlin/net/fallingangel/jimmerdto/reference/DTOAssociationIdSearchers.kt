54ff9f93958b6a9ad2e5e06d1070d3f40a62653d
FallingAngel <the.fallenangel.965@gmail.com>
2026-08-10 20:37:25 +0800

    perf(search): 将 IdView 引用搜索的预筛范围限定到 dto 文件

--- src/main/kotlin/net/fallingangel/jimmerdto/reference/DTOAssociationIdSearchers.kt
+++ src/main/kotlin/net/fallingangel/jimmerdto/reference/DTOAssociationIdSearchers.kt
@@ -3,12 +3,14 @@ package net.fallingangel.jimmerdto.reference
 import com.intellij.openapi.application.QueryExecutorBase
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiReference
+import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.search.SearchRequestCollector
 import com.intellij.psi.search.SearchScope
 import com.intellij.psi.search.UsageSearchContext
 import com.intellij.psi.search.searches.MethodReferencesSearch
 import com.intellij.psi.search.searches.ReferencesSearch
 import com.intellij.util.Processor
+import net.fallingangel.jimmerdto.core.DTOFileType
 import net.fallingangel.jimmerdto.lsi.jimmer.isReference
 import net.fallingangel.jimmerdto.lsi.process
 
@@ -41,6 +43,13 @@ private fun contribute(
     val lClass = process(clazz) { lClass() } ?: return
     val property = lClass.properties.find { it.source?.isEquivalentTo(element) == true } ?: return
     if (!property.isReference) return
+
+    val scope = scope.intersectWith(
+        GlobalSearchScope.getScopeRestrictedByFileTypes(
+            GlobalSearchScope.allScope(element.project),
+            DTOFileType.INSTANCE,
+        )
+    )
 
     optimizer.searchWord(
         "${property.name}Id",
