6b943dcb0364a7af6cc190b95064b8806aa5d1cc
FallingAngel <the.fallenangel.965@gmail.com>
2026-08-11 11:34:59 +0800

    refactor(disposable): 删除 DTOPluginDisposable 的 disposed 标志

--- src/main/kotlin/net/fallingangel/jimmerdto/DTOPluginDisposable.kt
+++ src/main/kotlin/net/fallingangel/jimmerdto/DTOPluginDisposable.kt
@@ -7,14 +7,10 @@ import com.intellij.openapi.project.Project
 
 @Service(Service.Level.PROJECT)
 class DTOPluginDisposable : Disposable {
-    @Volatile
-    var disposed: Boolean = false
-
-    companion object {
-        fun getInstance(project: Project): DTOPluginDisposable = project.service<DTOPluginDisposable>()
+    override fun dispose() {
     }
 
-    override fun dispose() {
-        disposed = true
+    companion object {
+        fun getInstance(project: Project) = project.service<DTOPluginDisposable>()
     }
 }
