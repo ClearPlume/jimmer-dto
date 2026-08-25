3fc215eceafc2d06731c0566aed2a9509e3ac46f
FallingAngel <the.fallenangel.965@gmail.com>
2026-08-25 09:49:47 +0800

    fix(resolve): ImportEntry.target 改为惰性求值

--- src/main/kotlin/net/fallingangel/jimmerdto/psi/resolve/ImportEntry.kt
+++ src/main/kotlin/net/fallingangel/jimmerdto/psi/resolve/ImportEntry.kt
@@ -1,5 +1,6 @@
 package net.fallingangel.jimmerdto.psi.resolve
 
+import com.intellij.psi.PsiNamedElement
 import net.fallingangel.jimmerdto.psi.element.DTOAlias
 import net.fallingangel.jimmerdto.psi.element.DTOImportStatement
 import net.fallingangel.jimmerdto.psi.element.DTOImportedType
@@ -8,9 +9,10 @@ import net.fallingangel.jimmerdto.psi.mixin.DTOElement
 class ImportEntry(val qualifiedName: String, val alias: DTOAlias?, val declaration: DTOElement) {
     val simpleName = alias?.value ?: qualifiedName.substringAfterLast('.')
 
-    val target = when (declaration) {
-        is DTOImportStatement -> declaration.qualifiedName.target?.source?.let { alias ?: it }
-        is DTOImportedType -> declaration.type.target?.source?.let { alias ?: it }
-        else -> null
-    }
+    val target: PsiNamedElement?
+        get() = when (declaration) {
+            is DTOImportStatement -> declaration.qualifiedName.target?.source?.let { alias ?: it }
+            is DTOImportedType -> declaration.type.target?.source?.let { alias ?: it }
+            else -> null
+        }
 }
